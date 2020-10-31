42e55988af32085c0717cfd8492781e8d87742b1
Roman <r.bova@mail.ru>
2020-10-31 04:42:17 +0300

    Adding database suppott

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+import psycopg2
 
 app = Flask(__name__)
+DB_URL = "postgres://aqutuzrfdzsdeg:fecd7ec0c96cd4494dc1a99334a6bfef46ce26fd7906cfe341d5925994b2a989@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/dcteppef0ebqt9"
 
-@app.route('/')
-def index():
-    return 'Cilui Github CI\CD ;)'
+connection = psycopg2.connect(DATABASE_URL, sslmode="require")
+
+@app.route('/recommendations/<int: user_id>', methods=["GET"])
+def get_recommendations_by_user_id(user_id):
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM recomendations;")
+    
+    result = cursor.fetchall()
+    
+    return jsonify([{"info1": row["INFO1"], "info2": row["INFO2"] from row in result}])
