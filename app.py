aec1bf72109551e9cecf01caae2307bbcbcbca32
Roman <r.bova@mail.ru>
2020-10-31 04:54:34 +0300

    Adding database suppott

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 import psycopg2
 
 app = Flask(__name__)
-DB_URL = "postgres://aqutuzrfdzsdeg:fecd7ec0c96cd4494dc1a99334a6bfef46ce26fd7906cfe341d5925994b2a989@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/dcteppef0ebqt9"
+DATABASE_URL = "postgres://aqutuzrfdzsdeg:fecd7ec0c96cd4494dc1a99334a6bfef46ce26fd7906cfe341d5925994b2a989@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/dcteppef0ebqt9"
 
 connection = psycopg2.connect(DATABASE_URL, sslmode="require")
 
