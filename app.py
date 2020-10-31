a46283b3b73e664ec3ecafe0a0d6f2e887bc4e91
Roman <r.bova@mail.ru>
2020-10-31 05:36:07 +0300

    Adding database suppott

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify
+from flask-cors import CORS, cross_origin
 import psycopg2
 import os
 
 app = Flask(__name__)
+CORS(app)
 DATABASE_URL = os.environ["DATABASE_URL"]
 
 connection = psycopg2.connect(DATABASE_URL, sslmode="require")
 
 @app.route('/recommendations/<int:user_id>', methods=["GET"])
+@cross_origin()
 def get_recommendations_by_user_id(user_id):
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM recomendations;")
